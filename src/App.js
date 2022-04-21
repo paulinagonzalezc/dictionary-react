d542d87b6e0d321bdc902e65f2b5d1fe99aca33b
Paulina Gonzalez <paulinagzzc@gmail.com>
2022-04-20 23:29:52 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import sky from "./purple-sky.png";
+
+function App() {
+  return (
+    <div className="App">
+      <header>Hello</header>
+      <img src={sky} className="sky" alt="sky" />
+    </div>
+  );
+}
+
+export default App;
