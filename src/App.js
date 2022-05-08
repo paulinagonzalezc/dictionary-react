cf41f5a9489e79e6576ee49f1c81eb8c2ac4f83f
Paulina Gonzalez <paulinagzzc@gmail.com>
2022-05-08 12:09:26 -0500

    Changed default word

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
       </div>
 
       <div className="container dictionary">
-        <Dictionary defaultKeyword="sunrise" />
+        <Dictionary defaultKeyword="sunset" />
       </div>
       <footer className="reference">
         <a
