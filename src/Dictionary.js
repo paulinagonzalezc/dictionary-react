a1c12732606fdb072b95271704a5bf304e8ba8af
Paulina Gonzalez <paulinagzzc@gmail.com>
2023-07-28 19:01:01 -0700

    Fix images api

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,17 +1,16 @@
-import React, { useState } from "react";
-import axios from "axios";
-import Results from "./Results.js";
-import Photos from "./Photos.js";
-import "./Dictionary.css";
+import React, {useState} from 'react';
+import axios from 'axios';
+import Results from './Results.js';
+import Photos from './Photos.js';
+import './Dictionary.css';
 
-export default function Dictionary(props) {
-  let [keyword, setKeyword] = useState(props.defaultKeyword);
+export default function Dictionary() {
+  let [keyword, setKeyword] = useState('sunset');
   let [results, setResults] = useState(null);
   let [loaded, setLoaded] = useState(false);
   let [photos, setPhotos] = useState(null);
 
   function handleDictionaryResponse(response) {
-    // console.log(response.data[0]);
     setResults(response.data[0]);
   }
 
@@ -23,12 +22,17 @@ export default function Dictionary(props) {
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
     axios.get(apiUrl).then(handleDictionaryResponse);
 
-    let pexelsApiKey =
-      "563492ad6f917000010000011a396fa64aab4d59950e84d7288fb234";
-    let headers = { Authorization: `Bearer ${pexelsApiKey}` };
+    let shecodesKey = '157ed0b5eb96387a704f272o2f9ct6b8';
 
-    let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=4`;
-    axios.get(pexelsApiUrl, { headers: headers }).then(handlePexelsResponse);
+    let shecodesUrl = `https://api.shecodes.io/images/v1/search?query=${keyword}&key=${shecodesKey}`;
+    axios.get(shecodesUrl).then(handlePexelsResponse);
+
+    // let pexelsApiKey =
+    //   '563492ad6f917000010000011a396fa64aab4d59950e84d7288fb234';
+    // let headers = {Authorization: `Bearer ${pexelsApiKey}`};
+
+    // let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=4`;
+    // axios.get(pexelsApiUrl, {headers: headers}).then(handlePexelsResponse);
   }
 
   function handleSubmit(event) {
@@ -59,6 +63,6 @@ export default function Dictionary(props) {
     );
   } else {
     load();
-    return "Loading";
+    return 'Loading';
   }
 }
--- src/Photos.js
+++ src/Photos.js
@@ -1,14 +1,13 @@
-import "./Photos.css";
+import './Photos.css';
 
 export default function Photos(props) {
-  console.log(props);
   if (props.photos) {
     return (
       <div className="Photos">
         <div className="row">
           {props.photos.map(function (photo, index) {
             return (
-              <div className="col-3" key={index}>
+              <div className="col-3 photo" key={index}>
                 <a href={photo.src.original} target="_blank" rel="noreferrer">
                   <img
                     src={photo.src.landscape}
